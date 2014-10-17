f0247ac58cc62980ce9744c132779da65ac5aad3
koeppent <theres.koeppen@hp.com>
2014-10-17 15:49:24 +0200

    AppCOnfig

---
+++ pictureserver/src/main/java/de/dhbw/pictureshow/servlet/AppConfig.java
@@ -0,0 +1,13 @@
+package de.dhbw.pictureshow.servlet;
+
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.ApplicationPath;
+
+/**
+ * Created by koeppent on 17.10.2014.
+ */
+
+@ApplicationPath("/rest")
+public class AppConfig extends Application {
+}
